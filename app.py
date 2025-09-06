7c7598e37d85c3d1e5624f55dd4e2b54bcadb6eb
Nishaojha1 <its.nishaojha@gmail.com>
2025-09-06 18:41:26 +0530

    how to save the data

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
 #read data from CSV file into a data frame
-df = pd.read_csv("sales_data_sample.csv", encoding="latin1")
-print(df)
+# df = pd.read_csv("sales_data_sample.csv", encoding="latin1")
+# print(df)
+#read data from Excel file into a data frame
+# df = pd.read_excel("SampleSuperstore.xlsx")
+# print(df)
+# #read data from Json file into a data frame
+# df = pd.read_json("sample_Data.json")
+# print(df)
